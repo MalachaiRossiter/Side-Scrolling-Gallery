59a4701c8bff129527147e2c0ba2d11390bc5137
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-08-11 14:05:06 -0400

    side scrolling works as well as mouse changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-
+// Making the navbar work 
 $(".navbutton-container").click(function(){
     $(".navbar").css({
         width: "16%"
@@ -9,29 +9,32 @@ $(".navbutton-container").click(function(){
 
     setTimeout(function(){
         $(".majorLinks").css({
-            opacity: "1"
+            opacity: "1",
+            pointerEvents: "all"
         });
         $(".minorLinks").css({
-            opacity: "1"
+            opacity: "1",
+            pointerEvents: "all"
         });
         $(".close").css({
-            opacity: "1"
+            opacity: "1",
+            pointerEvents: "all"
         });
     },500);
 });
 
 $(".close").click(function(){
     $(".majorLinks").css({
-        opacity: "0"
-    });
-    $(".majorLinks").css({
-        opacity: "0"
+        opacity: "0",
+        PointerEvent: "none"
     });
     $(".minorLinks").css({
-        opacity: "0"
+        opacity: "0",
+        PointerEvent: "none"
     });
     $(".close").css({
-        opacity: "0"
+        opacity: "0",
+        PointerEvent: "none"
     });
     $("#copiedEmail").css({
         visibility: "hidden"
@@ -44,10 +47,29 @@ $(".close").click(function(){
             width: "3%"
         });
     },250);
+    
 });
 
 //Copy to ClipBoard
 function copyClipboard(){
-    navigator.clipboard.writeText("malachai.rossiter@gmail.com");
+    navigator.clipboard.writeText("tinkeringartist@gmail.com");
     document.getElementById("copiedEmail").setAttribute("style", "display: inline-block;");
 }
+
+// getting cursor to follow you
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", e => {
+    cursor.setAttribute("style", "top: " +(e.pageY-3)+ "px; left: "+(e.pageX-3)+"px;");
+    if (e.target.classList.contains("linkitem") || e.target.classList.contains("navdot"))  {
+        cursor.classList.add("cursorLink");
+        cursor.setAttribute("style", "top: " +(e.pageY-11)+ "px; left: "+(e.pageX-11)+"px;");
+    } else {
+        cursor.classList.remove("cursorLink");
+    }
+    if (e.target.classList.contains("navbar")){
+        console.log("Something");
+        $(".cursor").css({
+            backgroundColor: "#000"
+        });
+    }
+})
